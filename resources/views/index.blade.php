81da093590543d6683d9c16cb04df406ed8f2274
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-16 17:56:31 +0200

    Refactor index view to use Blade components for movie display

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Movies</title>
-</head>
-<body>  
-    
-        @foreach ($movies as $movie)
+{{-- Layout --}}
+@extends('layouts/master')
 
-        <div class="card">
-            <div class="card-body">
-                <h2>{{ $movie['title'] }}</h2>
-                <h3>{{ $movie['original_title'] }}</h3>
-                <p>Data: {{ $movie['date'] }}</p>
-                <p>Nazionalità: {{ $movie['nationality'] }}</p>
-                <p>Voto: {{ $movie['vote'] }}</p>            
-            </div>
-        </div>
-            
-        
-            
-        @endforeach
-    
-</body>
-</html>
+{{-- Contenuto di index --}}
+@section('main')
+<main>  
+    {{-- Card --}}
+    @foreach ($movies as $movie)
+    <x-card>
+        <x-slot:title>{{ $movie['title'] }}</x-slot>
+        <x-slot:original_title>{{ $movie['original_title'] }}</x-slot>
+        <x-slot:date>Data: {{ $movie['date'] }}</x-slot>
+        <x-slot:nationality>Nazionalità: {{ $movie['nationality'] }}</x-slot>
+        <x-slot:vote>Voto: {{ $movie['vote'] }}</x-slot>
+    </x-card>
+    @endforeach
+</main>
+@endsection
