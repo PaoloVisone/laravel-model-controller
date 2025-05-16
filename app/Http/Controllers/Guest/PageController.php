58c9a7e6574422a155ed8a778b82e79ca34f4f40
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-16 16:52:41 +0200

    Implement PageController with index method to fetch and display movies

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        // Dati del database Movies
+        $movies = Movie::all();
+
+        return view('index', compact('movies'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rotta che restituisce PageController
+Route::get('/', [PageController::class, 'index']);
