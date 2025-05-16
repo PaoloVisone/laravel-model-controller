5217c5ec1809c7b1e7a87ec56f2dd76a9955d563
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-16 17:56:10 +0200

    Add card component for movie display

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,11 @@
+{{-- Card --}}
+        <div class="card">
+            <div class="card-body">
+                <h2>{{ $title }}</h2>
+                <h3>{{ $original_title }}</h3>
+                <p>{{ $date }}</p>
+                <p>{{ $nationality }}</p>
+                <p>{{ $vote }}</p>            
+            </div>
+        </div>
+             
